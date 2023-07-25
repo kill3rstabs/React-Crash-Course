29e8455d69fc80c7a1bbdda9d76a07d793de6e18
kill3rstabs <nashitnoorali78@gmail.com>
2023-07-25 07:47:09 +0000

    Reactdom first commit

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,58 +1,79 @@
 import './App.css';
-import { Header } from "./Components/Header.js";
-import { Todos } from './Components/Todos';
+import Header from "./Components/Header";
+import { Todos } from "./Components/Todos";
 import { Footer } from "./Components/Footer";
-import React, { useState, useEffect } from 'react';
 import { AddTodo } from "./Components/AddTodo";
+import { About } from "./Components/About";
+import React, { useState, useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
-  // Load todos from local storage if available, otherwise set default todos
-  //JSON.PARSE is used to convert the json into js object
-  const initialTodos = JSON.parse(localStorage.getItem('todos'))
-  //ye use state hai phele intial state define krtey hain phr jesey hi change hota tou setTodos ka function call krkey rerender krsktey
-  const [todos, setTodos] = useState(initialTodos);
+  let initTodo;
+  if (localStorage.getItem("todos") === null) {
+    initTodo = [];
+  }
+  else {
+    initTodo = JSON.parse(localStorage.getItem("todos"));
+  }
 
 
-  //use effect takes two parameters one is a callback function and other is dependencies.
-  //Kis variable mein change aye ga tou ye useEffect chaley ga.
-  //ager koi dependency nahi rakhi tou har render honey pe useEffect chaley ga which is a bad practice|takes up browsers load.
-  useEffect(() => {
-    // Save todos to local storage whenever todos state changes
-    //json.stringfy is used to convert js object into json
-    localStorage.setItem('todos', JSON.stringify(todos));
-  }, [todos]);
-  
+  const onDelete = (todo) => {
+    console.log("I am ondelete of todo", todo);
+    // Deleting this way in react does not work
+    // let index = todos.indexOf(todo);
+    // todos.splice(index, 1);
 
-  function onDelete(todo) {
-    console.log("I am onDelete!", todo);
     setTodos(todos.filter((e) => {
       return e !== todo;
     }));
+    console.log("deleted", todos)
+    localStorage.setItem("todos", JSON.stringify(todos));
   }
 
   const addTodo = (title, desc) => {
-    console.log("I am a todo ", title, " ", desc);
+    console.log("I am adding this todo", title, desc)
     let sno;
     if (todos.length === 0) {
-      sno = 1;
-    } else {
+      sno = 0;
+    }
+    else {
       sno = todos[todos.length - 1].sno + 1;
     }
     const myTodo = {
       sno: sno,
       title: title,
       desc: desc,
-    };
+    }
     setTodos([...todos, myTodo]);
     console.log(myTodo);
-  };
+  }
+
+  const [todos, setTodos] = useState(initTodo);
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos])
 
   return (
     <>
-      <Header title="To do List" searchBar={true} />
-      <AddTodo addTodo={addTodo} />
-      <Todos todos={todos} onDelete={onDelete} />
-      <Footer />
+      <Router>
+        <Header title="My Todos List" searchBar={false} />
+        <Switch>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/">
+            <>
+              <AddTodo addTodo={addTodo} />
+              <Todos todos={todos} onDelete={onDelete} />
+            </>
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
     </>
   );
 }
---
+++ react-task-tracker/src/Components/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem obcaecati molestias, necessitatibus ipsum dolore libero voluptates, dolorum voluptatibus totam, asperiores inventore minus? Molestiae hic vel odit dignissimos impedit soluta accusantium!</p>
+    </div>
+  )
+}
